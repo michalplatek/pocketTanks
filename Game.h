7f97304ee1961c29c5c239bb989cc2bdf5bafdb1
michalplatek <michal.platek.hf@gmail.com>
2015-05-13 17:26:53 +0200

    Game creation, storage and setup added

--- Game.h
+++ Game.h
@@ -1,9 +1,26 @@
 #pragma once
+#include "World.h"
 #include "Tank.h"
+#include "Config.h"
+#include <vector>
+
 class Game
 {
+	Config* config;
+	World* world;
+	std::vector<Tank*> tanks;
+	
+	Config::Status status;
+
 public:
-	Game();
+	Game(Config* config);
 	~Game();
+	// perform an iteration of the world
+	void step();
+	Config::Status getStatus();
+
+	void setStatus(Config::Status status);
+	World* getWorld();
+	Tank* getTank(int i);
 };
 
