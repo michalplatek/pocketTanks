98a081de7a832828a4420f8fe1a1c498aad75319
michalplatek <michal.platek.hf@gmail.com>
2015-05-13 17:31:33 +0200

    Box2D properties added to the world

--- World.h
+++ World.h
@@ -1,8 +1,11 @@
 #pragma once
-class World
+#include <Box2D/Box2D.h>
+#include "Config.h"
+class World : public b2World
 {
+	b2Body* body;
 public:
-	World();
+	World(Config* config);
 	~World();
 };
 
