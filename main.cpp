27630169b687c1c5702285ec72b9263f0c5a1fe3
michalplatek <michal.platek.hf@gmail.com>
2015-05-13 17:28:07 +0200

    Memory management; config added

--- main.cpp
+++ main.cpp
@@ -7,14 +7,18 @@
 #include "Config.h"
 using namespace std;
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
 	Config* config = new Config(argc, argv);
 	Controller* controller = new Controller(config);
-	Game* pocketTanks = new Game();
+	Game* pocketTanks = new Game(config);
 	
 	// Let's play Pocket Tanks!
 	controller->play(pocketTanks);
 	
+	delete config;
+	delete pocketTanks;
+	delete controller;
 	return 0;
 }
 
