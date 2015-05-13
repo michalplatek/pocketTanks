8a4544df28b24812a853308898a67bea2240ae39
michalplatek <michal.platek.hf@gmail.com>
2015-05-13 17:25:08 +0200

    Rendering code removed, key management added

--- Controller.cpp
+++ Controller.cpp
@@ -12,88 +12,47 @@ Controller::~Controller()
 {
 }
 
-void Controller::play(Game* game) {
+void Controller::play(Game* game)
+{
 	sf::Window *window;
 	sf::Event event;
 	window = view->getWindow();
 
-	b2Vec2 gravity = b2Vec2(0.0f, -0.1f);
-
-	b2World* world = new b2World(gravity);
-
-	b2BodyDef bodyDef;
-	bodyDef.type = b2_dynamicBody;
-	bodyDef.position.Set(0.0f, 4.0f);
-	b2Body* body = world->CreateBody(&bodyDef);
-
-	b2PolygonShape dynamicBox;
-	dynamicBox.SetAsBox(1.0f, 1.0f);
-
-	b2FixtureDef fixtureDef;
-	fixtureDef.shape = &dynamicBox;
-	fixtureDef.density = 1.0f;
-	fixtureDef.friction = 0.3f;
-
-	body->CreateFixture(&fixtureDef);
-	int32 velocityIterations = 6;
-	int32 positionIterations = 2;
-	float32 timeStep = 1.0f / 30.0f;
-
-	Tank* tank = new Tank(world, 1);
-
-	bool running = true;
-	while (running)
+	while (game->getStatus() == Config::Status::RUNNING)
 	{
-		//window->clear(sf::Color::White);
-		
-		glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-		glMatrixMode(GL_PROJECTION);
-		//gluOrtho2D(0.0, 200.0, 0.0, 150.0);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		view->prepare();
+		game->step();		
 
-		world->Step(timeStep, velocityIterations, positionIterations);
-		b2Vec2 position = tank->m_body->GetPosition();
-		float32 angle = tank->m_body->GetAngle();
-		tank->renderAtBodyPosition();
-		printf("%4.2f %4.2f %4.2f\n", position.x, position.y, angle);
-		//printf("%4.2f %4.2f %4.2f\n", position.x, position.y, angle);
-		
-		glBegin(GL_LINES);
-		glColor3f(1.0f,0.0f,0.f);
-		glLineWidth(3);
-		glVertex2f(0,0);
-		glVertex2f(0.9f, 0.9f);
-		glEnd();
+		view->render(game);
+
+		/* render test lines */
+		view->renderTest();
+
 
 		// manage window events
 		while (window->pollEvent(event))
 		{
-			switch (event.type) {
-			case sf::Event::Closed:
-
-				// ending the game
-				/*
-				if (game->getWinner() == NONE) {
-					game->saveGamestate();
-				}
-				*/
-				running = false;
-				break;
-			case sf::Event::KeyPressed:
-
-				// key was pressed
-				manageKeyEvent(event, game);
-
-			default:
-				break;
-			}
+			manageEvent(event, game);
 		}
-
 		window->display();
 	}
 }
 
-void Controller::manageKeyEvent(sf::Event &e, Game *game) {
+void Controller::manageEvent(sf::Event &e, Game* game)
+{
+	switch (e.type) {
+	case sf::Event::Closed:
+		game->setStatus(Config::Status::FINISHED);
+		break;
+	case sf::Event::KeyPressed:
+		manageKeyEvent(e, game);
+	default:
+		break;
+	}
+}
+
+void Controller::manageKeyEvent(sf::Event &e, Game *game)
+{
 	/*short actionCode = KEY_OTHER;
 	short player = 8;
 	switch (e.key.code) {
