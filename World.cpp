a66d71a6c6684818e5b76d580a6bf6136ce6025d
michalplatek <michal.platek.hf@gmail.com>
2015-05-13 17:31:16 +0200

    Box2D properties added to the World

--- World.cpp
+++ World.cpp
@@ -1,11 +1,27 @@
 #include "World.h"
 
 
-World::World()
+World::World(Config* config) : b2World(b2Vec2(0.0f, config->GRAVITY))
 {
+
+	b2BodyDef bodyDef;
+	bodyDef.type = b2_dynamicBody;
+	bodyDef.position.Set(0.0f, 4.0f);
+	body = this->CreateBody(&bodyDef);
+
+	b2PolygonShape dynamicBox;
+	dynamicBox.SetAsBox(1.0f, 1.0f);
+
+	b2FixtureDef fixtureDef;
+	fixtureDef.shape = &dynamicBox;
+	fixtureDef.density = 1.0f;
+	fixtureDef.friction = 0.3f;
+
+	body->CreateFixture(&fixtureDef);
 }
 
 
 World::~World()
 {
+	this->DestroyBody(body);
 }
