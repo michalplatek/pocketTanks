55fac898238a01ecbed86646cd9b7f853f6d6665
michalplatek <michal.platek.hf@gmail.com>
2015-05-16 19:16:33 +0200

    Renderable method implementations added

--- Tank.cpp
+++ Tank.cpp
@@ -1,4 +1,5 @@
 #include "Tank.h"
+#include "Renderable.h"
 
 Tank::Tank(World* world)
 {
@@ -24,4 +25,49 @@ Tank::Tank(World* world)
 Tank::~Tank()
 {
 	
+}
+
+void Tank::render() {
+	glColor3f(1, 1, 1);//white
+
+	//nose and eyes
+	glPointSize(4);
+	glBegin(GL_POINTS);
+	glVertex2f(0.0f, 0.0f);
+	glVertex2f(-0.5f, 0.5f);
+	glVertex2f(0.5f, 0.5f);
+	glEnd();
+
+	//mouth
+	glBegin(GL_LINES);
+	glVertex2f(-0.5f, -0.5f);
+	glVertex2f(-0.16f, -0.6f);
+	glVertex2f(0.16f, -0.6f);
+	glVertex2f(0.5f, -0.5f);
+	glEnd();
+
+	//circle outline
+	glBegin(GL_LINE_LOOP);
+	for (float a = 0; a < 360 * 3.1415f / 180; a += 30 * 3.1415f / 180)
+		glVertex2f(sinf(a), cosf(a));
+	glEnd();
+
+	b2Vec2 vel = body->GetLinearVelocity();
+	float red = vel.Length() / 20.0f;
+	red = fmin(1, red);
+	glColor3f(red, 0.5, 0.5);
+}
+
+void Tank::renderAtBodyPosition()
+{
+	//get current position from Box2D
+	b2Vec2 pos = body->GetPosition();
+	float angle = body->GetAngle();
+
+	//call normal render at different position/rotation
+	glPushMatrix();
+	glTranslatef(pos.x, pos.y, 0);
+	glRotatef(angle * 180 / 3.1415f, 0, 0, 1);//OpenGL uses degrees here
+	render();//normal render at (0,0)
+	glPopMatrix();
 }
