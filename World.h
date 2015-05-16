8a141e5cc931406949f60ea823d7e3bd21dd1572
michalplatek <michal.platek.hf@gmail.com>
2015-05-16 19:17:05 +0200

    Renderable interface added

--- World.h
+++ World.h
@@ -1,11 +1,16 @@
 #pragma once
 #include <Box2D/Box2D.h>
 #include "Config.h"
-class World : public b2World
+#include "Renderable.h"
+
+class World : public b2World, public Renderable
 {
 	b2Body* body;
 public:
 	World(Config* config);
 	~World();
+
+	void render();
+	void renderAtBodyPosition();
 };
 
