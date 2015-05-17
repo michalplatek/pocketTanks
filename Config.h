c010b7a059c3fc2023148fbad614d707079e8de1
michalplatek <michal.platek.hf@gmail.com>
2015-05-17 15:46:12 +0200

    Keyboard config added

--- Config.h
+++ Config.h
@@ -1,5 +1,6 @@
 #ifndef _CONFIG_H_
 #define _CONFIG_H_
+#include<SFML/Window.hpp>
 
 class Config
 {
@@ -12,22 +13,38 @@ public:
 	char* WINDOW_TITLE = "Pocket Tanks";
 	int ARGC;
 	char** ARGV;
+
 	/* sfml contect configuration */
 	int WINDOW_DEPTH_BITS;
 	int WINDOW_STENCIL_BITS;
 	int WINDOW_ANTIALIASING_LEVEL;
 	int WINDOW_MAJOR_VERSION;
 	int WINDOW_MINOR_VERSION;
+
 	/* box2d object configurations */
 	int BOX2D_POSITION_ITERATIONS;
 	int BOX2D_VELOCITY_ITERATIONS;
 	float BOX2D_TIME_STEP;
+
 	/* game settings */
 	int NUM_PLAYERS;
 	int TANKS_PER_PLAYER;
 	int NUM_TANKS;
 	float GRAVITY;
 	enum class Status { RUNNING, FINISHED };
+
+	/* keyboard controls */
+	int KEY_PLAYER1_UP;
+	int KEY_PLAYER1_DOWN;
+	int KEY_PLAYER1_LEFT;
+	int KEY_PLAYER1_RIGHT;
+	int KEY_PLAYER1_FIRE;
+	int KEY_PLAYER2_UP;
+	int KEY_PLAYER2_DOWN;
+	int KEY_PLAYER2_LEFT;
+	int KEY_PLAYER2_RIGHT;
+	int KEY_PLAYER2_FIRE;
+	int KEY_QUIT;
 
 	Config(int argc, char** argv);
 	~Config();
