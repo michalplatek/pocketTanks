1098faea6d13f62d22868e734396a4dbba1da182
michalplatek <michal.platek.hf@gmail.com>
2015-05-19 14:09:43 +0200

    renderAtBodyPos() deleted, accessors added

--- Renderable.h
+++ Renderable.h
@@ -1,11 +1,25 @@
-#pragma once
+#ifndef _RENDERABLE_H_
+#define _RENDERABLE_H_
+#include <Box2D/Box2D.h>
+
 class Renderable
 {
+	b2Body* body;
+	b2World* world;
+
 public:
 	Renderable();
 	virtual ~Renderable();
-
 	virtual void render() = 0;
-	virtual void renderAtBodyPosition() = 0;
+
+	b2Body* getBody();
+	void setBody(b2Body* body);
+
+	b2World* getWorld();
+	void setWorld(b2World* world);
+
+	b2Vec2 getBodyPosition();
+	float getBodyAngle();
 };
 
+#endif
