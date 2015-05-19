cbf86b0c4eaa8fa188c54b643c5369dbe701d108
michalplatek <michal.platek.hf@gmail.com>
2015-05-19 14:11:09 +0200

    Methods and accessord added to Renderable
    Now there are b2Body and b2World members in the Renderable class, also
    accessors that return and change them, and methods for getting body
    properties

--- Renderable.cpp
+++ Renderable.cpp
@@ -9,3 +9,34 @@ Renderable::Renderable()
 Renderable::~Renderable()
 {
 }
+
+b2Body* Renderable::getBody()
+{
+	return body;
+}
+
+void Renderable::setBody(b2Body* body)
+{
+	this->body = body;
+}
+
+b2World* Renderable::getWorld()
+{
+	return world;
+}
+
+void Renderable::setWorld(b2World* world)
+{
+	this->world = world;
+}
+
+b2Vec2 Renderable::getBodyPosition()
+{
+	return getBody()->GetPosition();
+}
+
+float Renderable::getBodyAngle()
+{
+	return getBody()->GetAngle();
+}
+
