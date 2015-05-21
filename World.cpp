ce8079a4eeba7a90a9f0c54d6744469c9db58dac
michalplatek <michal.platek.hf@gmail.com>
2015-05-21 17:16:25 +0200

    New world shape and rendering method (green box)

--- World.cpp
+++ World.cpp
@@ -10,15 +10,21 @@ World::World(Config* config) : Renderable(config)
 	b2Body* body = NULL;
 
 	b2BodyDef bodyDef;
-	bodyDef.type = b2_dynamicBody;
-	bodyDef.position.Set(100.0f, 50.0f);
+	bodyDef.type = b2_staticBody;
+	bodyDef.position.Set(100.0f, 10.0f);
 	body = world->CreateBody(&bodyDef);
 
-	b2PolygonShape dynamicBox;
-	dynamicBox.SetAsBox(100.0f, 10.0f);
+	b2Vec2 vertices[4];
+	vertices[0].Set(-100.0, 5.0f);
+	vertices[1].Set(100.0, 5.0f);
+	vertices[2].Set(100.0, -5.0f);
+	vertices[3].Set(-100.0, -5.0f);
+	int32 verticesCount = 4;
+	b2PolygonShape worldShape;
+	worldShape.Set(vertices, verticesCount);
 
 	b2FixtureDef fixtureDef;
-	fixtureDef.shape = &dynamicBox;
+	fixtureDef.shape = &worldShape;
 	fixtureDef.density = 0.0f;
 	fixtureDef.friction = 0.3f;
 
@@ -37,34 +43,44 @@ World::~World()
 void World::render()
 {
 	b2Body* body = getBody();
-	glColor3f(1, 0, 0);//red
 
-	//nose and eyes
-	glPointSize(4);
-	glBegin(GL_POINTS);
-	glVertex2f(0.0f, 0.0f);
-	glVertex2f(-0.5f, 0.5f);
-	glVertex2f(0.5f, 0.5f);
-	glEnd();
+	/*
+	Use GetVertexCount() and GetVertex() to get the vertices from a polygon shape.
 
-	//mouth
-	glBegin(GL_LINES);
-	glVertex2f(-0.5f, -0.5f);
-	glVertex2f(-0.16f, -0.6f);
-	glVertex2f(0.16f, -0.6f);
-	glVertex2f(0.5f, -0.5f);
-	glEnd();
+	Note that vertex positions stored in the fixture are in body coordinates
+	(relative to the body that the fixture is attached to).
+	To get locations in world coordinates, you would have to multiply by the body transform:
 
-	//circle outline
-	glBegin(GL_LINE_LOOP);
-	for (float a = 0; a < 360 * 3.1415f / 180; a += 30 * 3.1415f / 180)
-		glVertex2f(sinf(a)*10, cosf(a)*10);
-	glEnd();
+	b2Vec2 worldPos = body->GetWorldPoint( localPos );
+	*/
 
-	b2Vec2 vel = body->GetLinearVelocity();
-	float red = vel.Length() / 20.0f;
-	red = fmin(1.0f, red);
-	glColor3f(red, 0.5, 0.5);
+	b2Fixture* fixture;
+
+	for (fixture = body->GetFixtureList(); fixture; fixture = fixture->GetNext())
+	{
+		b2Shape::Type shapeType = fixture->GetType();
+		if (shapeType == b2Shape::e_polygon)
+		{
+			b2PolygonShape* polygonShape = (b2PolygonShape*)fixture->GetShape();
+			int vertexCount = polygonShape->GetVertexCount();
+			b2Vec2 vertex, vertexPositionInWorld;
+
+			glColor3f(0.1f, 1.0f, 0.0f);//green
+			glBegin(GL_QUADS);
+			for (int i = 0; i < vertexCount; i++)
+			{
+				vertex = polygonShape->GetVertex(i);
+				float posx = getConfig()->positionToPixel(vertex.x);
+				float posy = getConfig()->positionToPixel(vertex.y);
+
+				if (i == 0) printf("%f %f\n", posx, posy);
+
+				glVertex2f(posx, posy);
+
+			}
+			glEnd();
+		}
+	}
 }
 
 
