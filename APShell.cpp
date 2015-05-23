0c05128bad9b025bbd570f40ca55c3f66197ef75
michalplatek <michal.platek.hf@gmail.com>
2015-05-23 03:22:39 +0200

    APShell method implementations updated
    player parameter added to the APShell constructor.
    Fixture and shape creation added.
    Rendering method added.

--- APShell.cpp
+++ APShell.cpp
@@ -2,12 +2,60 @@
 
  // Armour Piercing Shell
 
-APShell::APShell(b2World* world, Config* config, b2Vec2 position, float angle) : Shell(world, config, position, angle)
+APShell::APShell(b2World* world, Config* config, Config::Players player, b2Vec2 position, float angle) : Shell(world, config, player, position, angle)
 {
+	b2Body* body = getBody();
 
+	//add polygon fixture
+	b2Vec2 vertices[4];
+	vertices[0].Set(0.1f, 0.2f);
+	vertices[1].Set(0.1f, -0.2f);
+	vertices[2].Set(-0.1f, -0.2f);
+	vertices[3].Set(-0.1f, 0.2f);
+	int32 verticesCount = 4;
+	b2PolygonShape APShellShape;
+	APShellShape.Set(vertices, verticesCount);
+
+	FixtureData* userData = new FixtureData;
+	userData->fixtureType = Config::FixtureType::SHELL;
+	userData->owner = player;
+
+	b2FixtureDef myFixtureDef;
+	myFixtureDef.shape = &APShellShape;
+	myFixtureDef.density = 1.0f;
+	myFixtureDef.userData = (void*)userData;
+	body->CreateFixture(&myFixtureDef);
 }
-
 
 APShell::~APShell()
 {
+}
+
+void APShell::render()
+{
+	b2Body* body = getBody();
+	b2Fixture* fixture;
+
+	for (fixture = body->GetFixtureList(); fixture; fixture = fixture->GetNext())
+	{
+		b2Shape::Type shapeType = fixture->GetType();
+		if (shapeType == b2Shape::e_polygon)
+		{
+			b2PolygonShape* polygonShape = (b2PolygonShape*)fixture->GetShape();
+			int vertexCount = polygonShape->GetVertexCount();
+			b2Vec2 vertex, vertexPositionInWorld;
+
+			glColor3f(0.9f, 0.9f, 0.9f);//light gray
+			glBegin(GL_QUADS);
+			for (int i = 0; i < vertexCount; i++)
+			{
+				vertex = polygonShape->GetVertex(i);
+				float posx = getConfig()->positionToPixel(vertex.x);
+				float posy = getConfig()->positionToPixel(vertex.y);
+
+				glVertex2f(posx, posy);
+			}
+			glEnd();
+		}
+	}
 }
