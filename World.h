96380b7bff4895f6045f701f7e61647acee5f937
michalplatek <michal.platek.hf@gmail.com>
2015-05-25 01:02:04 +0200

    Boost::Geometry added to World class #2

--- World.h
+++ World.h
@@ -10,12 +10,10 @@
 #include <boost/geometry/geometries/geometries.hpp>
 #include <boost/geometry/geometries/point_xy.hpp>
 #include <boost/geometry/geometries/register/ring.hpp>
-#include <boost/geometry/geometries/register/point.hpp> 
+#include <boost/geometry/geometries/register/point.hpp>
 
-namespace bg = boost::geometry;
-
-BOOST_GEOMETRY_REGISTER_POINT_2D(b2Vec2, float, bg::cs::cartesian, x, y)
-typedef model::ring<b2Vec2, false, true> ring_t;
+BOOST_GEOMETRY_REGISTER_POINT_2D(b2Vec2, float, boost::geometry::cs::cartesian, x, y)
+typedef boost::geometry::model::ring<b2Vec2, false, true> ring_t;
 
 class World : public Renderable
 {
