d56b8b167b34a6a3636572fba2094cb483f9c0a3
michalplatek <michal.platek.hf@gmail.com>
2015-05-29 15:21:41 +0200

    explosion added to shell

--- Shell.cpp
+++ Shell.cpp
@@ -3,12 +3,12 @@
 #include "World.h"
 #include "Tank.h"
 
-Shell::Shell(b2World* world, Config* config, Config::Players player, b2Vec2 position, float angle) : Renderable(config)
+Shell::Shell(b2World* world, Config* config, Config::Players player, Config::ShellType shellType, b2Vec2 position, float angle)
+	: Renderable(config), shellType(shellType), explosion(nullptr)
 {
 	setWorld(world);
 
-	b2Body* body;
-	body = NULL;
+	b2Body* body = nullptr;
 
 	BodyData* bodyData = new BodyData;
 	bodyData->bodyType = Config::BodyType::SHELL;
@@ -29,7 +29,7 @@ Shell::Shell(b2World* world, Config* config, Config::Players player, b2Vec2 posi
 	float vectorY = sin(angle * DEGTORAD);
 	printf("cos(%f)= %f, sin(%f)= %f\n", angle, vectorX, angle, vectorY);
 	//body->SetTransform(b2Vec2(position.x, position.y), angle);
-	body->ApplyLinearImpulse(b2Vec2(-vectorX * 30.0f, vectorY * 30.0f), b2Vec2(0.0f, 0.0f), true);
+	body->ApplyLinearImpulse(b2Vec2(-vectorX * config->SHELL_VELOCITY, vectorY * config->SHELL_VELOCITY), b2Vec2(0.0f, 0.0f), true);
 
 	setBody(body);
 }
@@ -39,7 +39,6 @@ bool Shell::collision()
 	b2Body* body = getBody();
 	for (b2ContactEdge* edge = body->GetContactList(); edge; edge = edge->next)
 	{
-
 		if (edge->contact->IsTouching())
 		{
 			return true;
@@ -50,5 +49,36 @@ bool Shell::collision()
 
 Shell::~Shell()
 {
+	if (explosion != nullptr)
+	{
+		getWorld()->DestroyBody(explosion);
+	}
+	if (getBody() != nullptr)
+	{
+		getWorld()->DestroyBody(getBody());
+	}
+}
+
+void Shell::explode()
+{
+	explosion = nullptr;
+	//set up dynamic body, store in class variable
+	b2BodyDef myBodyDef;
+	myBodyDef.type = b2_dynamicBody;
+	// last known position of the shell
+	myBodyDef.position = getBody()->GetPosition();
+	explosion = getWorld()->CreateBody(&myBodyDef);
+
+	b2CircleShape explosionShape;
+	explosionShape.m_p.Set(0.0f, 0.0f);
+	explosionShape.m_radius = getConfig()->EXPLOSION_RADIUS[shellType];
+
+	b2FixtureDef myFixtureDef;
+	myFixtureDef.shape = &explosionShape;
+	myFixtureDef.density = 0;
+	myFixtureDef.isSensor = true;
+	explosion->CreateFixture(&myFixtureDef);
+
 	getWorld()->DestroyBody(getBody());
+	setBody(nullptr);
 }
