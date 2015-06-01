3fd89d512c842a1ed0a6af2c81b99b40dfb6d289
michalplatek <michal.platek.hf@gmail.com>
2015-06-01 13:57:08 +0200

    Chain shape in World. Rendering updated accordingly

--- World.cpp
+++ World.cpp
@@ -20,21 +20,22 @@ World::World(Config* config) : Renderable(config)
 	bodyDef.userData = (void*)bodyData;
 	body = world->CreateBody(&bodyDef);
 
-	b2Vec2 vertices[4];
-	vertices[0].Set(-500.0, 5.0f);
-	vertices[1].Set(500.0, 5.0f);
-	vertices[2].Set(500.0, -5.0f);
-	vertices[3].Set(-500.0, -5.0f);
-	int32 verticesCount = 4;
-	b2PolygonShape worldShape;
-	worldShape.Set(vertices, verticesCount);
+	// Add a destructible polygon
+	std::vector<b2Vec2> vertices = {
+		b2Vec2{ -500.0f, 5.0f },
+		b2Vec2{ 500.0f, 5.0f },
+		b2Vec2{ 500.0f, -5.0f },
+		b2Vec2{ -500.0f, -5.0f } 
+	};
+
+	auto worldShape = makeChain(vertices.data(), vertices.size(), true);
 
 	FixtureData* userData = new FixtureData;
 	userData->fixtureType = Config::FixtureType::WORLD;
 	userData->owner = Config::Players::NONE;
 
 	b2FixtureDef fixtureDef;
-	fixtureDef.shape = &worldShape;
+	fixtureDef.shape = worldShape.get();
 	fixtureDef.density = 0.0f;
 	fixtureDef.friction = 0.3f;
 	fixtureDef.userData = (void*)userData;
@@ -43,6 +44,19 @@ World::World(Config* config) : Renderable(config)
 	setBody(body);
 }
 
+std::unique_ptr<b2ChainShape> World::makeChain(b2Vec2* points, int count, bool closed)
+{
+	std::unique_ptr<b2ChainShape> shape{ new b2ChainShape() };
+	if (closed)
+	{
+		shape->CreateLoop(points, count);
+	}
+	else
+	{
+		shape->CreateChain(points, count);
+	}
+	return shape;
+}
 
 World::~World()
 {
@@ -73,6 +87,7 @@ void World::render()
 		b2Shape::Type shapeType = fixture->GetType();
 		if (shapeType == b2Shape::e_polygon)
 		{
+			printf("a polygon\n");
 			b2PolygonShape* polygonShape = (b2PolygonShape*)fixture->GetShape();
 			int vertexCount = polygonShape->GetVertexCount();
 			b2Vec2 vertex, vertexPositionInWorld;
@@ -82,6 +97,30 @@ void World::render()
 			for (int i = 0; i < vertexCount; i++)
 			{
 				vertex = polygonShape->GetVertex(i);
+				float posx = getConfig()->positionToPixel(vertex.x);
+				float posy = getConfig()->positionToPixel(vertex.y);
+
+				//if (i == 0) printf("%f %f\n", posx, posy);
+
+				glVertex2f(posx, posy);
+
+			}
+			glEnd();
+		}
+		else if (shapeType == b2Shape::e_chain)
+		{
+			printf("a chain\n");
+			b2ChainShape* chainShape = (b2ChainShape*)fixture->GetShape();
+			int edgeCount = chainShape->GetChildCount();
+			b2Vec2 vertex, vertexPositionInWorld;
+
+			glColor3f(0.1f, 1.0f, 0.0f);//green
+			glBegin(GL_QUADS);
+			for (int i = 0; i < edgeCount; i++)
+			{
+				b2EdgeShape edge;
+				chainShape->GetChildEdge(&edge, i);
+				vertex = edge.m_vertex1;
 				float posx = getConfig()->positionToPixel(vertex.x);
 				float posy = getConfig()->positionToPixel(vertex.y);
 
