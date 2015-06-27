75fe2524569f38cfa8b23e46c1517f5783224f84
Kasia <duszak1993@gmail.com>
2015-06-27 02:14:57 +0200

    added new files: Text for displaying text in gui field, Shader for making text and FreeGlutHelper for kepping glew and freeglut

---
+++ FreeGlutHelper.h
@@ -0,0 +1,9 @@
+#ifndef _FREEGLUTHELPER_H_
+#define _FREEGLUTHELPER_H_
+
+// GLEW
+#include <GL/glew.h>
+
+#include <GL/freeglut.h>
+
+#endif
---
+++ Text.h
@@ -0,0 +1,29 @@
+#ifndef _TEXT_H_
+#define _TEXT_H_
+
+
+// GLM
+#include <glm.hpp>
+#include <matrix_transform.hpp>
+#include <type_ptr.hpp>
+// FreeType
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
+#include "Config.h"
+#include <map>
+#include "Renderable.h"
+#include "FreeGlutHelper.h"
+#include "Shader.h"
+
+
+class Text : public Renderable {
+public:
+	Text(Config *config);
+	~Text();
+	void render();
+	void renderText(Shader &shader, std::string text, GLfloat x, GLfloat y, GLfloat scale, glm::vec3 color);
+	void configureText();
+};
+
+#endif
